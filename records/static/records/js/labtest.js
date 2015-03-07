a3fbede3e65bd1fcb7d1e63cbc0f7636ceda437e
NISH1001 <nish_play1001@yahoo.com>
2015-03-07 18:53:53 +0545

    serology fields added

--- records/static/records/js/labtest.js
+++ records/static/records/js/labtest.js
@@ -30,10 +30,32 @@ function selectionNumeric(id)
 
 function selectionBoolean(id)
 {
-    alert(id);
+    var numeric = document.getElementById('boolean');
+    var x = document.getElementById(id);
+    numeric.removeChild(x);
 
-    var selected = document.getElementById("selected");
+    var sel_text = "form_"+id;
+
+    var newdiv = document.createElement("div");
+    newdiv.setAttribute('id', sel_text);
+
+    var label = document.createElement("LABEL");
+    var t = document.createTextNode(sel_text);
+    label.appendChild(t);
+
+    var button = document.createElement("input");
+    button.type = "button";
+    button.value = "cancel";
+    button.addEventListener("click", function () { cancelClick(sel_text); } );
+
+    newdiv.appendChild(label);
+    newdiv.appendChild(button);
+
+    var divappend = document.getElementById("selected");
+    divappend.appendChild(newdiv);
+
 }
+
 
 function cancelClick(id)
 {
