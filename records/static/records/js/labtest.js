40b3edcb05301610221c79d66e4cf002b30a072e
NISH1001 <nish_play1001@yahoo.com>
2015-03-07 20:30:10 +0545

    field selection improved

--- records/static/records/js/labtest.js
+++ records/static/records/js/labtest.js
@@ -1,52 +1,63 @@
 
-
+// selection on click method
+// numeric form creation is to be done using this
 function selectionNumeric(id)
 {
-    var numeric = document.getElementById('numeric');
+    //get our  node with id that is to be hidden after selection
     var x = document.getElementById(id);
-    numeric.removeChild(x);
+    //numeric.removeChild(x);
+    x.style.visibility = "hidden";
 
-    var sel_text = "form_"+id;
+    var form_id = "form_"+id;
 
+    //create a new div
     var newdiv = document.createElement("div");
-    newdiv.setAttribute('id', sel_text);
+    newdiv.setAttribute('id', form_id );
 
+    //create label
     var label = document.createElement("LABEL");
-    var t = document.createTextNode(sel_text);
+
+    //extract the name part
+    var t = document.createTextNode(id);
     label.appendChild(t);
 
+    //create button
     var button = document.createElement("input");
     button.type = "button";
     button.value = "cancel";
-    button.addEventListener("click", function () { cancelClick(sel_text); } );
+    button.addEventListener("click", function () { cancelClick(form_id); } );
 
+    //append
     newdiv.appendChild(label);
     newdiv.appendChild(button);
 
+
+    //append to our div
     var divappend = document.getElementById("selected");
     divappend.appendChild(newdiv);
 
 }
 
+// radio button type of form to be created here
 function selectionBoolean(id)
 {
-    var numeric = document.getElementById('boolean');
+    //get our  node with id that is to be hidden after selection
     var x = document.getElementById(id);
-    numeric.removeChild(x);
+    x.style.visibility = "hidden";
 
-    var sel_text = "form_"+id;
+    var form_id = "form_"+id;
 
     var newdiv = document.createElement("div");
-    newdiv.setAttribute('id', sel_text);
+    newdiv.setAttribute('id', form_id);
 
     var label = document.createElement("LABEL");
-    var t = document.createTextNode(sel_text);
+    var t = document.createTextNode(id);
     label.appendChild(t);
 
     var button = document.createElement("input");
     button.type = "button";
     button.value = "cancel";
-    button.addEventListener("click", function () { cancelClick(sel_text); } );
+    button.addEventListener("click", function () { cancelClick(form_id); } );
 
     newdiv.appendChild(label);
     newdiv.appendChild(button);
@@ -59,7 +70,14 @@ function selectionBoolean(id)
 
 function cancelClick(id)
 {
-    alert(id);
+    //remove 'form_' part from form id
+    var duplicate = id.slice(5,id.length);
+    var splitted = duplicate.split(" ");
+    var type = splitted[0];
+
+    var x = document.getElementById(duplicate);
+    x.style.visibility = "visible";
+
     var x = document.getElementById(id);
     x.remove();
 }
