96e5a7aa7ce3dfcdf85dad6842064a0cb4372249
NISH1001 <nish_play1001@yahoo.com>
2015-03-07 14:32:54 +0545

    Haematology fields added

---
+++ records/migrations/0006_auto_20150307_0741.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('records', '0005_auto_20150307_0340'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='test',
+            name='reportDate',
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 7, 7, 41, 24, 840175)),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='visit',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 7, 7, 41, 24, 839641)),
+            preserve_default=True,
+        ),
+    ]
